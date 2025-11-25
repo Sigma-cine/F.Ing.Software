45002a83502e636c3c9c90c47dc0c3bbb2c1bf66
Valeria Herrera <valeria.herrera.arb@gmail.com>
2025-11-25 06:56:59 -0500

    Corección BD

--- src/main/java/sigmacine/infraestructura/configDataBase/DatabaseConfig.java
+++ src/main/java/sigmacine/infraestructura/configDataBase/DatabaseConfig.java
@@ -9,9 +9,7 @@ public final class DatabaseConfig {
     private static final String USER   = "sa";
     private static final String PASS   = "";
 
-    //private static final String URL ="jdbc:h2:file:./sigmacine/db/cine_db;DB_CLOSE_ON_EXIT=FALSE;AUTO_RECONNECT=TRUE";
-   // private static final String URL ="jdbc:h2:file:~/sigmacine/db/cine_db;AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE";
-    private static final String URL ="jdbc:h2:~/sigmacine/db/cine_db;AUTO_SERVER=TRUE;DB_CLOSE_DELAY=-1";
+    private static final String URL ="jdbc:h2:file:./sigmacine/db/cine_db;DB_CLOSE_ON_EXIT=FALSE;AUTO_RECONNECT=TRUE";
 
     private static final String DRIVER = "org.h2.Driver";
 
