7025ac6a4fd1e03d3d0309d12a25518b655d48dc
Carolina Ujueta <c_ujueta@javeriana.edu.co>
2025-11-26 22:46:39 -0500

    limpiar entity

--- src/main/java/sigmacine/dominio/entity/Funcion.java
+++ src/main/java/sigmacine/dominio/entity/Funcion.java
@@ -2,14 +2,10 @@ package sigmacine.dominio.entity;
 
 import java.sql.Date;
 import java.sql.Time;
-import java.util.List;
 
 public class Funcion {
 
     private long id;
-    private List<Pelicula> trailers;
-    private Pelicula pelicula;
-    private Sala sala;
     private Date fecha;
     private Time hora;
     private String estado;
